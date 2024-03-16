8423b49ee782beb1cb4651a8e345b831ed511e8c
Kaarti22 <kaartimondi@gmail.com>
2024-03-16 09:25:28 +0530

    finished feed section

--- client/src/components/FeedPosts/FeedPosts.jsx
+++ client/src/components/FeedPosts/FeedPosts.jsx
@@ -1,16 +1,50 @@
-import { Container } from '@chakra-ui/react'
-import React from 'react'
-import FeedPost from './FeedPost'
+import {
+  Box,
+  Container,
+  Flex,
+  Skeleton,
+  SkeletonCircle,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import FeedPost from "./FeedPost";
 
 const FeedPosts = () => {
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsloading(false);
+    }, 2000);
+  }, []);
+
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
-        <FeedPost/>
-        <FeedPost/>
-        <FeedPost/>
-        <FeedPost/>
+      {isLoading &&
+        [0, 1, 2, 3].map((_, idx) => {
+          <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+            <Flex gap={2}>
+              <SkeletonCircle size="10" />
+              <VStack gap={2} alignItems={"flex-start"}>
+                <Skeleton height={"10px"} w={"200px"} />
+                <Skeleton height={"10px"} w={"200px"} />
+              </VStack>
+            </Flex>
+            <Skeleton w={"full"}>
+              <Box h={"500px"}> Contents Wrapped </Box>
+            </Skeleton>
+          </VStack>;
+        })}
+      {!isLoading && (
+        <>
+          <FeedPost img="/img1.png" username="Kaarti" avatar="/img1.png" />
+          <FeedPost img="/img2.png" username="Kaarti" avatar="/img2.png" />
+          <FeedPost img="/img3.png" username="Kaarti" avatar="/img3.png" />
+          <FeedPost img="/img4.png" username="Kaarti" avatar="/img4.png" />
+        </>
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default FeedPosts
+export default FeedPosts;
